5c0e83faa2b7a6164ecb407b6b4bed324f4f7eaa
calvnce <intels_arcade@outlook.com>
2021-08-11 16:32:46 +0300

    Add recursive approach

--- maxsubarray/script.js
+++ maxsubarray/script.js
@@ -59,4 +59,54 @@ class MaximumSubArray{
     }
     return bestSum;
   }
+
+  maxCrossingSubArray(A, low, mid, high) {
+    let bestSumBelowMidArray = -Number.MAX_SAFE_INTEGER,
+        bestSumAboveMidArray = -Number.MAX_SAFE_INTEGER;
+    let belowMidMaxIndex = 0,
+        aboveMidMaxIndex = 0;
+
+    let currentSum = 0;
+
+    for (let index = mid; index >= low; index -= 1) {
+      currentSum += A[index];
+      if(currentSum > bestSumBelowMidArray){
+        bestSumBelowMidArray = currentSum;
+        belowMidMaxIndex = index;
+      }
+    }
+
+    currentSum = 0;
+    for (let index = mid + 1; index <= high; index += 1) {
+      currentSum += A[index];;
+      if(currentSum > bestSumAboveMidArray){
+        bestSumAboveMidArray = currentSum;
+        aboveMidMaxIndex = index;
+      }
+    }
+
+    return [belowMidMaxIndex,aboveMidMaxIndex, (bestSumBelowMidArray + bestSumAboveMidArray)];
+  }
+
+  recurviveMaxSubArray(A,low,high) {
+    if (low === high){
+      return [low, high, A[low]];
+    }
+    else {
+      let mid = Math.floor((low + high) / 2);
+      const subArrayBelow = this.recurviveMaxSubArray(A,low,mid);
+      const subArrayAbove = this.recurviveMaxSubArray(A, mid + 1, high);
+      const subArrayCrossMid = this.maxCrossingSubArray(A, low, mid, high);
+
+      if (subArrayBelow[2] >= subArrayAbove[2] && subArrayBelow[2] >= subArrayCrossMid[2]){
+        return subArrayBelow;
+      }
+      else if (subArrayAbove[2] >= subArrayBelow[2] && subArrayAbove[2] >= subArrayCrossMid[2]) {
+        return subArrayAbove;
+      }
+      else {
+        return subArrayCrossMid;
+      }
+    }
+  }
 }
